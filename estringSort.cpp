a9f8f9d79ac19093dd35fb8658e88ddbfb404b6d
vivek-sundli <33592335+vivek-sundli@users.noreply.github.com>
2021-08-10 23:39:44 +0530

    Update estringSort.cpp
    extended

--- estringSort.cpp
+++ estringSort.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+/*
 bool cmp(string s0,string s1){
 	if(s0 < s1){
 			s0==s1?return false:return true;
@@ -8,7 +8,30 @@ bool cmp(string s0,string s1){
 	}
 	return false;
 }
+*/
+int cmp(string s1, string s2) {
 
+        int i = 0;      
+
+        while (i < s1.length() && i < s2.length()) {
+
+            if (s1[i] > s2[i]) {
+
+                return 1;
+            } else if (s1[i] < s2[i]) {
+                return -1;
+            }
+            i++;
+
+        }
+
+        if (s1.length() > s2.length()) {
+            return -1;
+        } else {
+            return 1;
+        }
+
+    }
 int main() {
 	string s[100];
 	int t;
